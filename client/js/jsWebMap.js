07cd6707ca348b15cbca0c427e1ad75f24edcbf1
Justin Martinez <jmartinez@drcog.org>
2016-02-17 10:57:00 -0700

    added google maps api

--- client/js/jsWebMap.js
+++ client/js/jsWebMap.js
@@ -9,8 +9,21 @@ if(Meteor.isClient){
         },
         years: function() {
             return [2015,2016,2017]
+        }, testOptions: function(){
+            if(GoogleMaps.loaded()){
+                return{
+                    center: new google.maps.LatLng(39.9281049, -105.0209591),
+                    zoom:8,
+                    mapTypeId: google.maps.MapTypeId.HYBRID
+                };
+            }
         }
 
-    })
+
+    });
+
+    Template.webMap.onRendered(function(){
+        GoogleMaps.load();
+    });
 
 }
