960de0655dbc0a7914fe768594bd3056a1b98b03
Justin Martinez <justinmartinez14@gmail.com>
2016-05-17 10:54:07 -0600

    added google fonts

--- client/main.js
+++ client/main.js
@@ -3,3 +3,23 @@
  */
 import '../imports/startup/client/routes.js';
 import '../imports/startup/client/mapFunctions.js';
+if(Meteor.isClient){
+
+    Meteor.startup(function(){
+        WebFontConfig = {
+            google: {families: ['Lato:400', 'Lato:300', 'Mouse Memoirs']}
+        };
+        (function () {
+            var wf = document.createElement('script');
+            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+                '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+            console.log("async fonts loaded", WebFontConfig);
+        })();
+
+    })
+
+}
