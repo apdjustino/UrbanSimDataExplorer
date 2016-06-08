8e944ca9c41a09af8bcc3c5a34561ca8bfdc533a
Justin Martinez <justinmartinez14@gmail.com>
2016-06-08 07:35:14 -0600

    Fixed Bug BG-214: Invite email message is not clear

--- server/main.js
+++ server/main.js
@@ -17,6 +17,18 @@ if(Meteor.isServer){
         Accounts.urls.enrollAccount = function(token){
             return Meteor.absoluteUrl('enroll-account/' + token)
         };
+        
+        Accounts.emailTemplates.from = 'DRCOG Land Use Team <no-reply@drcog.org';
+        Accounts.emailTemplates.siteName = 'DRCOG Land Use Explorer';
+        Accounts.emailTemplates.enrollAccount.subject = function(user){
+            return user.profile.firstName + ' ' + user.profile.lastName + ', you have been invited to join the DRCOG Land Use Explorer';
+        };
+        
+        Accounts.emailTemplates.enrollAccount.text = function(user, url){
+            return "Hello " + user.profile.firstName + ", \n\n You are invited to join the DRCOG Land Use Explorer project. Your account will " +
+                "allow you review DRCOG's latest land use forecast, provide feedback, and download the results. To get started, click on the link" +
+                "below and set up your password. \n\n" + url; 
+        };
 
         //process.env.MAIL_URL = "smtp://justin%40sandbox4851f242dc32413caf7306f4f466e0d1.mailgun.org:destroy1@smtp.mailgun.org:587"
         process.env.MAIL_URL = "smtp://10.0.1.201:25"
