82c06761664726732013bf02c93f756e076609d2
Justin Martinez <jmartinez@drcog.org>
2016-07-05 07:15:06 -0600

    testing urbansim_buildings sub

--- imports/api/buildings/publications.js
+++ imports/api/buildings/publications.js
@@ -6,9 +6,10 @@ if(Meteor.isServer){
         return buildings_centroids.find({geometry: {$geoWithin:{$geometry:{ type: "Polygon", coordinates: [[[southWest.lng, southWest.lat], [southWest.lng, northEast.lat], [northEast.lng, northEast.lat], [northEast.lng, southWest.lat], [southWest.lng, southWest.lat]]]}}}});
     });
 
-    Meteor.publish('selected_building', function(plan_id){
-        console.log(plan_id);
-        console.log(urbansim_buildings.find({"plan_id": plan_id}).fetch());
-        return urbansim_buildings.find({"plan_id": plan_id})
+    Meteor.publish('selected_building', function(id){
+        console.log(id);
+        console.log(urbansim_buildings.findOne({"plan_id": id}));
+        console.log(urbansim_buildings.findOne());
+        return urbansim_buildings.find({"plan_id": id})
     });
 }
