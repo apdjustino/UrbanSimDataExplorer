eaff666953b63e5905307f313091ed7a48eb6fa0
Justin Martinez <jmartinez@drcog.org>
2016-07-05 11:19:06 -0600

    started making color changes for building click event

--- imports/ui/components/Global_helpers.js
+++ imports/ui/components/Global_helpers.js
@@ -35,7 +35,7 @@ if(Meteor.isClient){
     
     Template.registerHelper('formattedValue', function(val, measure){
         var str = measure.substr(measure.length - 3);
-        if(str !='_id' || str == 'year_built'){
+        if(str !='_id' || measure == 'year_built'){
             var format = d3.format("0,000");
             return format(val);
         }else{
