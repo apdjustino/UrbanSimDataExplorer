334a1c0f46d38c8ddd0fff9de671028ea8a03285
Justin Martinez <jmartinez@drcog.org>
2016-07-18 07:36:44 -0600

    changed formattedValue global helper for new parcel data

--- imports/ui/components/Global_helpers.js
+++ imports/ui/components/Global_helpers.js
@@ -37,7 +37,7 @@ if(Meteor.isClient){
         var str = measure.substr(measure.length - 3);
         if(str !='_id' || measure == 'year_built'){
             var format = d3.format("0,000");
-            if(!isNan(format(value))){
+            if(!isNaN(format(value))){
                 return format(val);
             }else{
                 return val;
