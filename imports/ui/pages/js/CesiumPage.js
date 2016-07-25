dcc740669777785a83036458af5da1dd61cf8cef
Justin Martinez <justinmartinez14@gmail.com>
2016-07-25 11:03:16 -0600

    testing loading buildings in cesium

--- imports/ui/pages/js/CesiumPage.js
+++ imports/ui/pages/js/CesiumPage.js
@@ -37,8 +37,26 @@ if(Meteor.isClient){
                         var ids = buildings_centroids.find({}).fetch().map(function(x){return x.properties.Building_I});
                         this.stop();
                         Meteor.call('findBuildings', ids, function(error, response){
-                            console.log(response);
-                        })
+                            var source = new Cesium.GeoJsonDataSource("buildings");
+                            viewer.dataSources.add(source);
+                            source.load({
+                                type: "FeatureCollection",
+                                crs: {
+                                    type: "name",
+                                    properties: {
+                                        name: "urn:ogc:def:crs:OGC:1.3:CRS84"
+                                    }
+                                },
+                                features: response
+                            });
+                            var entities = source.entities.values;
+                            for(var i =0; i<entities.length; i++) {
+                                var entity = entities[i];
+                                entity.polygon.extrudedHeight = entity.properties.Bldg_Heigh / 3.2;
+                                entity.polygon.material = Cesium.Color.BURLYWOOD;
+                                entity.polygon.outlineColor = Cesium.Color.BURLYWOOD;
+                            }
+                        });
                     }
                 });
 
