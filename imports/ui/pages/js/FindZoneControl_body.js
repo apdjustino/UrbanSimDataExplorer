fa5bd11d89a44caaa5c7bbc0f8f73ca0e66bd966
Justin Martinez <justinmartinez14@gmail.com>
2016-08-16 09:31:32 -0600

    Fixed zone finder

--- imports/ui/pages/js/FindZoneControl_body.js
+++ imports/ui/pages/js/FindZoneControl_body.js
@@ -19,24 +19,42 @@ if(Meteor.isClient){
     
     Template.FindZoneControl_body.events({
         "submit #findAZone": function(event, template){
+
             event.preventDefault();
+            var ds = viewer.dataSources.get(0);
             var zoneId = $('#zoneSearch').val();
-            Meteor.call("getZoneCentroid", zoneId, function(error, response){
-                if(error){
-                    sAlert.error(error.response, {position:"bottom"});
-                }else{
-                    $('.navLink').sideNav('hide');
-                    viewer.camera.flyTo({
-                        destination: Cesium.Cartesian3.fromDegrees(response[0], response[1], 4000),
-                        duration: 4,
-                        complete: function(){
-                            
-                            findZoneData(parseInt(zoneId), 2040);
-                            $('#infoModal').openModal();
-                        }
-                    });
-                }
-            })
+            var entity = _.find(ds.entities.values, function(x){return x.properties.zone_str == zoneId});
+            if(entity){
+                viewer.camera.flyTo({
+                    destination: Cesium.Cartesian3.fromDegrees(entity.properties.Long, entity.properties.Lat, 4000),
+                    duration: 4,
+                    complete: function(){
+                        findZoneData(zoneId, Session.get('selectedYear'));
+                    }
+                });
+
+            }else{
+                Materialize.toast("Zone: " + zoneId + " not found.", 4000);
+            }
+
+            
+            
+            // Meteor.call("getZoneCentroid", zoneId, function(error, response){
+            //     if(error){
+            //         sAlert.error(error.response, {position:"bottom"});
+            //     }else{
+            //         $('.navLink').sideNav('hide');
+            //         viewer.camera.flyTo({
+            //             destination: Cesium.Cartesian3.fromDegrees(response[0], response[1], 4000),
+            //             duration: 4,
+            //             complete: function(){
+            //
+            //                 findZoneData(parseInt(zoneId), 2040);
+            //                 $('#infoModal').openModal();
+            //             }
+            //         });
+            //     }
+            // })
         }
     })
     
