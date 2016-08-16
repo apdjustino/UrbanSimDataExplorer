2303051a0f191f72dc95ecceb4a97acec7a244b0
Justin Martinez <justinmartinez14@gmail.com>
2016-08-16 17:16:46 -0600

    Added reset button to the find menu

--- imports/ui/pages/js/OptionsControl_body.js
+++ imports/ui/pages/js/OptionsControl_body.js
@@ -10,7 +10,17 @@ if(Meteor.isClient){
             }else{
                 Session.set('allowMultipleGeo', false);
             }
+        }, "click #btnReset": function(event,template){
+            event.preventDefault();
+            Session.set('colorData', undefined);
+            Session.set('selectedData', undefined);
+            var ds = viewer.dataSources.get(0);
+            var entities = ds.entities.values;
+            entities.forEach(function(cv){
+                cv.polygon.material = new Cesium.Color(0.01,0.01,0.01,0.01);
+                cv.polygon.extrudedHeight = 0;
+            });
         }
     })
-    
+
 }
